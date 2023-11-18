df78045f3b8e020f16a150798510acc683f15fcf
Sudipta Basak <sudiptab2100@gmail.com>
2023-11-19 00:05:02 +0530

    Store into MongoDB

--- src/server.py
+++ src/server.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI, HTTPException
+from pymongo import MongoClient
 
 app = FastAPI()
+
+# Connect to MongoDB
+client = MongoClient("mongodb://localhost:27017")
+db = client["dyte"]  # Replace with your actual database name
+collection = db["dyte"]
 
 @app.post("/")
 async def handle_logs(data: dict):
     try:
         print("Received JSON data:")
         print(data)
+
+        # Insert the data into MongoDB
+        collection.insert_one(data)
 
         return {"message": "JSON data received successfully"}
     except Exception as e:
